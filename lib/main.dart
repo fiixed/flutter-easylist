bbd951dc1ecde953eb7099260c117b5d8acc5118
Andrew Bell <fiixed@gmail.com>
2019-01-07 12:04:38 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Text('Food Paradise'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
