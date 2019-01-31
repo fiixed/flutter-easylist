cf4d04b6cbd18b94601c0f9f2fc032faf8dec464
Andrew Bell <fiixed@gmail.com>
2019-01-31 10:01:09 -0600

    finished favorites

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import '../widgets/products/products.dart';
 
+import 'package:scoped_model/scoped_model.dart';
+
+import '../widgets/products/products.dart';
+import '../scoped-models/products.dart';
 
 class ProductsPage extends StatelessWidget {
-  
   Widget _buildSideDrawer(BuildContext context) {
     return Drawer(
       child: Column(
@@ -31,10 +33,18 @@ class ProductsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('EasyList'),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-          ),
+          ScopedModelDescendant<ProductsModel>(
+            builder: (BuildContext context, Widget child, ProductsModel model) {
+              return IconButton(
+                icon: Icon(model.displayFavoritesOnly
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () {
+                  model.toggleDisplayMode();
+                },
+              );
+            },
+          )
         ],
       ),
       body: Products(),
